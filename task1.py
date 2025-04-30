1990baefd2dec165349134d1053f0a4905aeb781
Seshu-adi <173929954+Seshu-adi@users.noreply.github.com>
2025-04-30 12:51:35 -0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+#Create a Personalized Greeting
+first=str(input('Enter your First Name:'))
+last=str(input('Enter your Last Name:'))
+
+#printing
+print('Hello,',first,last,'Welcome to the python program')
---
+++ task2.py
@@ -0,0 +1,15 @@
+# Perform Basic Mathematical Operations
+num1=int(input('Enter First Number:'))
+num2=int(input('Enter Second Number:'))
+
+#Caluclation
+add=num1+num2
+mult=num1*num2
+sub=num1-num2
+div=num1/num2
+
+#Printing
+print('Addition:',add)
+print('Multiplication:',mult)
+print('Subtraction:',sub)
+print('Division:',div)
