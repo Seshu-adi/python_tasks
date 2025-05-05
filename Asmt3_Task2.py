8fcd7f936018957df69cc0ad63f28530a88908ca
Seshu-adi <173929954+Seshu-adi@users.noreply.github.com>
2025-05-05 14:41:52 -0400

    Add files via upload

---
+++ Asmt3_Task2.py
@@ -0,0 +1,8 @@
+from math import sqrt
+from math import log
+from math import sin
+
+n=int(input('Enter a Number:'))
+print("Square root of ",n,"is:",sqrt(n))
+print("Logarithm of ",n,"is:",log(n))
+print("Sine of ",n,"is:",sin(n))
